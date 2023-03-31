b8c4522cb36d7ec912614b3271c0744d5585c2d1
Erin Timlin <erintimlin@gmail.com>
2023-03-31 13:01:31 -0400

    add dummy data to context

--- ParksApi/Models/ParksApiContext.cs
+++ ParksApi/Models/ParksApiContext.cs
@@ -9,5 +9,17 @@ namespace ParksApi.Models
     public ParksApiContext(DbContextOptions<ParksApiContext> options) : base (options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+        .HasData(
+          new Park { ParkId = 1, Name = "Zion National Park", State = "Utah", City = "Springdale" },
+          new Park { ParkId = 2, Name = "Yosemite National Park", State = "California", City = "Sacramento" },
+          new Park { ParkId = 3, Name = "Grand Canyon National Park", State = "Arizona", City = "Tusayan" },
+          new Park { ParkId = 4, Name = "Big Bend National Park", State = "Texas", City = "Big Bend" },
+          new Park { ParkId = 5, Name = "Yellowstone National Park", State = "Montana", City = "Gardiner"}
+        );
+    }
   }
 }
